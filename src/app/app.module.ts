5b419acbc00f35f1e3591b5275928d7bbc827993
Igr <igrflex@gmail.com>
2018-10-01 17:52:24 +0300

    Added start components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
+import { CellComponent } from './components/map/cell/cell.component';
+import { FieldComponent } from './components/field/field.component';
+import { PlayerFieldComponent } from './components/player-field/player-field.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    CellComponent,
+    FieldComponent,
+    PlayerFieldComponent,
+    HeaderComponent,
+    SideMenuComponent,
+    MainMenuComponent,
+    MainPageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule
