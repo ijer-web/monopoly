850a705fcd95c9e8b25383c901e35baf2802f0ff
Igr <igrflex@gmail.com>
2018-10-02 17:42:04 +0300

    Some renamed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
 import { CellComponent } from './components/map/cell/cell.component';
-import { FieldComponent } from './components/field/field.component';
-import { PlayerFieldComponent } from './components/player-field/player-field.component';
+import { FieldComponent } from './components/map/field/field.component';
+import { PlayerFieldComponent } from './components/map/player-field/player-field.component';
 import { HeaderComponent } from './components/header/header.component';
-import { SideMenuComponent } from './side-menu/side-menu.component';
-import { MainMenuComponent } from './main-menu/main-menu.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { LoginPageComponent } from './login-page/login-page.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {
+    path: 'mainPage',
+    component: MainPageComponent,
+  },
+  { path: '',
+    redirectTo: '/mainPage',
+    pathMatch: 'full'
+  },
+  { path: '**', component: MainPageComponent }
+];
+
 
 @NgModule({
   declarations: [
@@ -26,7 +40,8 @@ import { LoginPageComponent } from './login-page/login-page.component';
     LoginPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
